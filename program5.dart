79ba253755708ee9ae67760efee1ed392b159d38
SoftwareDeveloper-flutter <anilamuhammadashraf.software@gmail.com>
2024-08-31 23:34:36 +0500

    Assignment complete programs done

--- program5.dart
+++ program5.dart
@@ -1,9 +1,9 @@
 void main(){
-  int user_input_1=55;
-  if(user_input_1 % 5 ==0 && user_input_1 % 11 ==0){
-    print("The input number $user_input_1 is  divisible by 5 and 11");
+  int user_input_year=2000;
+  if(user_input_year % 100==0){
+    print("The input year $user_input_year is a century year.");
   } else {
-    print("The input number $user_input_1 is not divisible by 5 and 11.");
+    print("The input number $user_input_year is not a century year.");
   }
      
   
--- program8.dart
+++ program8.dart
@@ -1,10 +1,14 @@
 void main(){
-  int user_input=7;
-  if(user_input % 3 == 0 || user_input % 7 == 0){
-    print("The input number $user_input is multiple of 3 or 7");
-  } else {
-    print("The input number $user_input is not multiple of 3 or 7.");
-  }
+  int user_subject_1=50;
+  int user_subject_2=10;
+  int user_subject_3=20;
+  int user_subject_4=30;
+  int out_of=400;
+  var student_total_marks = user_subject_1 + user_subject_2 + user_subject_3 + user_subject_4;
+  var student_percentage = (student_total_marks / out_of) * 100 ;
+
+  print("Student's Total Marks:$student_total_marks");
+  print("Student's Percentage: $student_percentage" + "%");
      
   
   
