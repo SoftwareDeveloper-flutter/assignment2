6f30dd4a899d966e81f9269e56509e9390771c91
SoftwareDeveloper-flutter <anilamuhammadashraf.software@gmail.com>
2024-08-31 23:19:54 +0500

    more programs done

---
+++ program5.dart
@@ -0,0 +1,12 @@
+void main(){
+  int user_input_1=55;
+  if(user_input_1 % 5 ==0 && user_input_1 % 11 ==0){
+    print("The input number $user_input_1 is  divisible by 5 and 11");
+  } else {
+    print("The input number $user_input_1 is not divisible by 5 and 11.");
+  }
+     
+  
+  
+
+}
---
+++ program7.dart
@@ -0,0 +1,12 @@
+void main(){
+  int user_input=7;
+  if(user_input % 3 == 0 || user_input % 7 == 0){
+    print("The input number $user_input is multiple of 3 or 7");
+  } else {
+    print("The input number $user_input is not multiple of 3 or 7.");
+  }
+     
+  
+  
+
+}
